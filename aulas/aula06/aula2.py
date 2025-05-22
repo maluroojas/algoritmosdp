8a0ed1c2dd6b7a1d9d2f23e50bcff1d58d31d941
maluroojas <marialuisarojas666@gmail.com>
2025-05-21 22:45:59 -0400

    continuação

---
+++ aulas/aula06/aula2.py
@@ -0,0 +1,21 @@
+'''
+FUNÇÕES -----------------------------------------
+
+blocos de código identificados por um nome, podem receber "parametros"
+
+sintaxe:
+def func_num1(a, b):
+
+
+'''
+
+def soma (x, y):
+    soma = x + y 
+    return soma 
+
+a = int(input('digite um num'))
+b = int(input('digite outro num'))
+soma = soma(a,b)
+media = soma / 2
+
+
