c5a2a76ad39ea667dea950c2517d0289adaee341
maluroojas <marialuisarojas666@gmail.com>
2025-06-03 22:06:12 -0400

    dia 03

---
+++ aulas/aula10/aula.py
@@ -0,0 +1,26 @@
+'''
+def nome (x, y, z)
+    funcao
+    
+sintaxe: lambda argumentos: expressao
+'''
+
+dobro = lambda x: x * 2
+
+print('Exemplo - 1 Dobro de 5', dobro(5))
+
+soma = lambda x, y:  x + y 
+
+print('Exemplo - 2 Soma de 3 e 4', soma(3, 4))
+
+#formula da hipotenusa:
+hipotenusa_quadrado = lambda a, b: a**2 + b**2
+
+print ('Exemplo - 3 ', hipotenusa_quadrado(3, 4))
+
+#filter, map
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+pares = filter ()
+
